555bbd57431b0153ab766b631e38331b94e845c7
alexCoding42 <alexandre.cisse.ac@gmail.com>
2023-08-24 23:32:39 -0400

    fix: :bug: fix missing config for environment variables

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ["cdn.imagin.studio"],
   },
+  env: {
+    NEXT_PUBLIC_RAPID_API_KEY: process.env.NEXT_PUBLIC_RAPID_API_KEY,
+    NEXT_PUBLIC_IMAGIN_API_KEY: process.env.NEXT_PUBLIC_IMAGIN_API_KEY,
+  },
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_RAPID_API_KEY: process.env.NEXT_PUBLIC_RAPID_API_KEY,
+    NEXT_PUBLIC_IMAGIN_API_KEY: process.env.NEXT_PUBLIC_IMAGIN_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
